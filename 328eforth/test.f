461ef28a00f3a424c00fc073e4798dcbab5ce810
sam <samsuanchen@gmail.com>
2013-09-27 20:08:56 +0800

    modify

--- 328eforth/test.f
+++ 328eforth/test.f
@@ -1,9 +1,10 @@
+\ C:\ksanapc\atmega_ide\328eforth\test.f
 : t 8
   FOR CR 9 R@ - 8
     FOR DUP 9 R@ - x
     NEXT DROP
   NEXT ;
-t 
+  t 
 \ test.f
 DECIMAL
 \ FORGET V
@@ -15,14 +16,14 @@ VARIABLE V 1000 V !
 : F H W L W ;
 : .s CR DEPTH DUP ." DEPTH" . ?DUP IF ." --" 1- FOR R@ PICK . NEXT THEN ;
 \ FORGET Z
-: Z .s
+: Z
   BEGIN  F 6 EMIT ?KEY DUP
-    IF .s    ( k true ) OVER 17 =  \ is key ^Q  quick
-      IF     ( k true ) V @ 2/ ."  quick as" V ! 2DROP 0
-      ELSE   ( k true ) OVER 26 \ is key ^Z  easy
-        IF   ( k true ) V @ 2*  ."  easy as" V ! 2DROP 0
-        ELSE ( k true ) DROP 27 =  \ is key esc escape
-        THEN ( flag   )
+    IF          ( k true ) OVER 17 =  \ is key ^Q  quick
+      IF    .s  ( k true ) V @ 2/ ."  quick as" DUP . V ! 2DROP 0
+      ELSE      ( k true ) OVER 26 =  \ is key ^Z  easy
+        IF  .s  ( k true ) V @ 2*  ."  easy as" DUP . V ! 2DROP 0
+        ELSE    ( k true ) DROP 27 =  \ is key esc escape
+        THEN    ( flag   )
       THEN
     THEN
   UNTIL
